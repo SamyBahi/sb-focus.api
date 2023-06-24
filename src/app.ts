f4e2da28e2aff952873405f966ed8b717d74640e
Samy Bahi <samy.bahi.business@gmail.com>
2023-06-24 14:51:07 +0200

    added gzip compression

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,14 @@ import authRoutes from "./routes/auth";
 import tasksRoutes from "./routes/tasks";
 import listsRoutes from "./routes/lists";
 import cookieParser from "cookie-parser";
+import compression from "compression";
 import cors from "cors";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 
+app.use(compression());
 app.use(express.json());
 app.use(cookieParser());
 
