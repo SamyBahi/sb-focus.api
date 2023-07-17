df3920e1d16e61be9868161c99f57e122111980a
Samy Bahi <samy@samybahi.com>
2023-07-17 22:18:59 +0200

    trying a new fix for set cookie issue

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -53,7 +53,7 @@ export const loginController = async (
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: false,
-      sameSite: "none",
+      sameSite: "strict",
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
